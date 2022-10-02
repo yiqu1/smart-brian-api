0343e449afda4b9c7d31dd563d027a0b4ebba29c
yiqu1 <yqu82888@gmail.com>
2022-10-01 23:41:33 -0400

    'finish'

---
+++ controllers/image.js
@@ -0,0 +1,44 @@
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+const stub = ClarifaiStub.grpc();
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key c41631b45a964bc08ee51aa367a4e423");
+
+const handleApiCall = (req, res) => {
+  stub.PostModelOutputs(
+    {
+        model_id: "face-detection",
+        inputs: [{data: {image: {url: req.body.input}}}]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            console.log("Error: " + err);
+            return;
+        }
+
+        if (response.status.code !== 10000) {
+            console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+            return;
+        }
+
+        console.log("Predicted concepts, with confidence values:")
+        for (const c of response.outputs[0].data.concepts) {
+            console.log(c.name + ": " + c.value);
+        }
+        res.json(response)
+    }
+  );
+}
+
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body
+  db('users').where('id', '=', id).increment('entries', 1).returning('entries').then(entries => {
+    res.json(entries[0].entries);
+  }).catch(err=>res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
